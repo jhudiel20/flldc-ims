ab2850f55621831f4e227c4362f2f9fc1172b9dc
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-19 21:31:45 +0800

    trial

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php include 'config/config.php'; ?>
-<!doctype html>
+<!DOCTYPE html>
 
 <html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
     data-assets-path="<?php BASE_URL; ?>assets/" data-template="vertical-menu-template">
