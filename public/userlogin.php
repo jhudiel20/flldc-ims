d793abeb49ba304708cce685cdf8a828b03af6ac
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-20 13:28:49 +0800

    trial

--- public/userlogin.php
+++ public/userlogin.php
@@ -1,15 +1,4 @@
 <?php
-require 'DBConnection.php';
-include 'config/config.php';
+header('Content-Type: text/plain');
+var_dump($_REQUEST);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Start the session
-session_start();
-
-// Output the content of the $_REQUEST array to validate incoming data
-print_r($_REQUEST);
-exit();
-
-// The rest of your code...
