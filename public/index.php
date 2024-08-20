b21956648003073ae8a8749b549bbb7e58d352b8
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-20 17:31:57 +0800

    trial

--- public/index.php
+++ public/index.php
@@ -199,7 +199,7 @@ include 'config/config.php';
     });
 
     $('#login_btn').on('click', function() {
-        var formdata = new FormData(user_login);
+        var formdata = new FormData(user_login_form);
         $.ajax({
             url: <?php BASE_URL; ?> "action/userlogin.php",
             method: "POST",
