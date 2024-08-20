75adf79c758458fd115132010a090a5e4aa5cfb4
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-20 13:48:09 +0800

    trial

--- public/index.php
+++ public/index.php
@@ -175,6 +175,31 @@ include 'config/config.php';
       ?>
 </body>
 
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/jquery/jquery.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/popper/popper.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/js/bootstrap.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/hammer/hammer.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/i18n/i18n.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/typeahead-js/typeahead.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/js/menu.js"></script>
+
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/popular.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/bootstrap5.js"></script>
+        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/auto-focus.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <!-- Main JS -->
+        <script src="<?php BASE_URL; ?>assets/js/main.js"></script>
+
+        <!-- Page JS -->
+        <script src="<?php BASE_URL; ?>assets/js/pages-auth.js"></script>
+
+        <script src="<?php BASE_URL; ?>js/jquery-3.6.0.min.js?v=<?php echo FILE_VERSION; ?>"></script>
+
+
 <script>
     const Toast = Swal.mixin({
         toast: true,
@@ -227,28 +252,4 @@ include 'config/config.php';
         });
     });
 </script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/jquery/jquery.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/popper/popper.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/js/bootstrap.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/hammer/hammer.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/i18n/i18n.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/typeahead-js/typeahead.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/js/menu.js"></script>
-
-        <!-- endbuild -->
-
-        <!-- Vendors JS -->
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/popular.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/bootstrap5.js"></script>
-        <script src="<?php BASE_URL; ?>assets/vendor/libs/@form-validation/auto-focus.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <!-- Main JS -->
-        <script src="<?php BASE_URL; ?>assets/js/main.js"></script>
-
-        <!-- Page JS -->
-        <script src="<?php BASE_URL; ?>assets/js/pages-auth.js"></script>
-
-        <script src="<?php BASE_URL; ?>js/jquery-3.6.0.min.js?v=<?php echo FILE_VERSION; ?>"></script>
-
 </html>
