e943360ae9dfc43cf72280ec9b381c2d2080b8ac
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-21 11:33:20 +0800

    update vercel.json

--- api/userlogin.php
+++ api/userlogin.php
@@ -2,8 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require 'DBConnection.php';
-include 'config/config.php';
+require '../DBConnection.php';
 
 $response = array('status' => 'error', 'message' => '');
 
