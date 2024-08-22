604f98e407863bc875c108c739b01b21a5a58c1d
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-22 20:35:15 +0800

    dashborad & logout

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 require_once __DIR__ . '/../api/DBConnection.php';
 include 'config/config.php'; 
 
+if(isset($_COOKIE['status'])){
+    if($_COOKIE['status'] == true){
+    header("Location: /dashboard-lnd.php");
+    }
+  }
+
 ?>
 <!DOCTYPE html>
 
