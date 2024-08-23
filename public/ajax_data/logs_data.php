1a98d5420849a7ff00275bc2160ec5dfcc594bd2
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-23 20:10:14 +0800

    add tabulator folder

--- public/ajax_data/logs_data.php
+++ public/ajax_data/logs_data.php
@@ -2,6 +2,9 @@
 require_once __DIR__ . '/../DBConnection.php';
 require_once __DIR__ . '/../../public/config/config.php'; // Adjusted path for config.php
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 if (!(isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')){
     include HTTP_404;
     exit();
