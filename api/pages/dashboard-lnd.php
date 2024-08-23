1bfaab4b7846850fbae190fc73d124150b6dca96
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-23 18:17:46 +0800

    logs ajax data

--- api/pages/dashboard-lnd.php
+++ api/pages/dashboard-lnd.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../public/config/config.php'; // Adjusted path for config.php
-
+$decrypted_array = null;
 if (isset($_COOKIE['secure_data'])) {
     $decrypted_array = decrypt_cookie($_COOKIE['secure_data'], $encryption_key, $cipher_method);
 }
