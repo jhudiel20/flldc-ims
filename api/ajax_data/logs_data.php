5254c20b3412d516e8fc48d2e09d9756b20c9572
jhudiel20 <sueltojhudiel20@gmail.com>
2024-08-24 11:09:01 +0800

    trial

--- api/ajax_data/logs_data.php
+++ api/ajax_data/logs_data.php
@@ -6,11 +6,6 @@ error_reporting(0);
 ini_set('display_errors', 0);
 
 
-if (!(isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')){
-    include HTTP_404;
-    exit();
-}
-
 header("Content-type: application/json; charset=utf-8");
 
 $query_limit = 20;
