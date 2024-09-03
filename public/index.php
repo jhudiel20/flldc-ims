53669d7f49dd169ef6c15ab1ff00193cf8e1a1c5
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-03 11:42:01 +0800

    update request / print r cookie

--- public/index.php
+++ public/index.php
@@ -253,8 +253,8 @@ include 'config/config.php';
 
         $(document).ready(function() {
             $('#signin_btn').on('click', function() {
-                // var formdata = new FormData(document.getElementById('user_login_form'));
-                var formdata = new FormData(user_login_form);
+                var formdata = new FormData(document.getElementById('user_login_form'));
+                // var formdata = new FormData(user_login_form);
                 // e.preventDefault();  
 
                 $.ajax({
