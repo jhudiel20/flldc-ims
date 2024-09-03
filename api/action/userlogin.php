6f8c2a069d909fec3e3c7ba7ed84e354302cd15f
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-03 15:03:34 +0800

    purchase request

--- api/action/userlogin.php
+++ api/action/userlogin.php
@@ -87,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $decrypted_array = decrypt_cookie($_COOKIE['secure_data'], $encryption_key, $cipher_method);
                 }
 
-                if ($user['ACCESS'] == '') {
+                if ($decrypted_array['ACCESS'] == '') {
                     $response['icon'] = "info";
                     $response['success'] = false;
                     $response['title'] = "Error!";
@@ -95,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     echo json_encode($response);
                     exit();
                 }
-                if ($user['ACCESS'] == 'ENCODER' || $user['ACCESS'] == 'REQUESTOR') {
+                if ($decrypted_array['ACCESS'] == 'ENCODER' || $decrypted_array['ACCESS'] == 'REQUESTOR') {
                     if ($user['LOCKED'] == 3) {
                         $response['icon'] = "warning";
                         $response['success'] = false;
