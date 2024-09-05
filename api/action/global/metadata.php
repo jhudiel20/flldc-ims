a5a9d39c929f3e47b0add6e4c82d4563bd9b6199
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-05 10:53:10 +0800

    side bar

--- api/action/global/metadata.php
+++ api/action/global/metadata.php
@@ -2,6 +2,6 @@
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-<meta name="description" content="Learning & Development IMS">
+<meta name="description" content="Learning and Development IMS">
 <meta http-equiv="refresh" content="1800;../Actions" />
 <title><?php echo PAGE_TITLE; ?></title>
