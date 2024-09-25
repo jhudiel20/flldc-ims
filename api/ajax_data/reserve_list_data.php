e61c0c7be91c04c66afb7e03b17eecb9841e4fd0
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-25 16:48:34 +0800

    reservation list

--- api/ajax_data/reserve_list_data.php
+++ api/ajax_data/reserve_list_data.php
@@ -29,7 +29,7 @@ foreach ($filters as $filter) {
     if (isset($filter['field']) && isset($filter['value'])) {
             $field = $filter['field'];
             $value = '%' . $filter['value'] . '%';
-            $filter_clauses[] = "$field LIKE :$field";
+            $filter_clauses[] = "$field ILIKE :$field";
             $filter_params[$field] = $value;
     }
 }
