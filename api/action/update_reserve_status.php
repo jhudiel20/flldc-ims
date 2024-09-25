56bbd384d87647ec2f432d36002f6c1db5a901bd
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-25 14:00:32 +0800

    reservation list

--- api/action/update_reserve_status.php
+++ api/action/update_reserve_status.php
@@ -296,7 +296,8 @@ if ($counter->rowCount() > 0) {
             // Now, decline all overlapping pending reservations for the same room, date, and overlapping time slots
         $decline_pending = $conn->prepare("
             UPDATE reservations 
-            SET reserve_status = 'DECLINED' 
+            SET reserve_status = 'DECLINED', 
+            reservation_id = 'DECLINED'
             WHERE room = :room 
             AND reserve_date = :reserve_date
             AND reserve_status = 'PENDING'
@@ -312,6 +313,140 @@ if ($counter->rowCount() > 0) {
 
         // Execute the decline query
         $decline_pending->execute();
+
+
+        // Prepare to send email notifications for declined reservations
+        $decline_counter = $conn->prepare("
+            SELECT EMAIL FROM reservations 
+            WHERE room = :room 
+            AND reserve_date = :reserve_date
+            AND reserve_status = 'DECLINED'
+            AND time IN ($inClause)
+        ");
+        $decline_counter->bindParam(':room', $room, PDO::PARAM_STR);
+        $decline_counter->bindParam(':reserve_date', $reserve_date, PDO::PARAM_STR);
+
+        // Bind the overlapping time slots for the declined email notifications
+        foreach ($overlapping_times as $index => $time) {
+            $decline_counter->bindValue(':time' . $index, $time, PDO::PARAM_STR);
+        }
+
+        // Execute the query to get declined reservation emails
+        $decline_counter->execute();
+        $declined_emails = $decline_counter->fetchAll(PDO::FETCH_COLUMN);
+
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'lndreports2024@gmail.com';                     //SMTP username
+            $mail->Password   = 'yzmxbjcntuwkfdpe';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                           // TCP port to connect to
+
+            $mail->setFrom('lndreports2024@gmail.com', 'Learning and Development Inventory Management System');
+            foreach ($declined_emails as $declined_email) {
+                $mail->addAddress($declined_email);
+            }    //Add a recipient
+            $mail->addEmbeddedImage('/var/task/user/public/assets/img/LOGO.png', 'logo_cid');
+            //Content
+            $mail->isHTML(true); 
+            
+            $mail->Subject = 'Reservation Status Update: DECLINED';
+            $mail->Body    = '
+                <div style="background:#f3f3f3">
+                    <div style="margin:0px auto;max-width:640px;background:transparent">
+                    <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent" align="center" border="0">
+                        <tbody>
+                        <tr>
+                            <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 0px">
+                            <div aria-labelledby="mj-column-per-100" class="m_29934315870093561mj-column-per-100" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%">
+                                <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                <tbody>
+                                    <tr>
+                                    <td style="word-break:break-word;font-size:0px;padding:0px" align="center">
+                                        <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px" align="center" border="0">
+                                        <tbody>
+                                            <tr>
+                                            <td style="width:138px">
+                                                <img alt="" title="" height="100px" width="200px" src="cid:logo_cid" style="">
+                                            </td>
+                                            </tr>
+                                        </tbody>
+                                        </table>
+                                    </td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                            </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </div>
+        
+                    <div style="max-width:640px;margin:0 auto;border-radius:4px;overflow:hidden">
+                    <div style="margin:0px auto;max-width:640px;background:#fdfdfd">
+                        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#fdfdfd" align="center" border="0">
+                            <tbody>
+                                <tr>
+                                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 50px">
+                                        <div aria-labelledby="mj-column-per-100" class="m_29934315870093561mj-column-per-100" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style="word-break:break-word;font-size:0px;padding:0px" align="left">
+                                                            <div style="color:#737f8d;font-family:Whitney,Helvetica Neue,Helvetica,Arial,Lucida Grande,sans-serif;font-size:16px;line-height:24px;text-align:left">
+                                                                <h2 style="font-family:Whitney,Helvetica Neue,Helvetica,Arial,Lucida Grande,sans-serif;font-weight:500;font-size:20px;color:#4f545c;letter-spacing:0.27px">Hi good day,</h2>
+                                                                <p style="text-align:justify">Your reservation request has been declined. Below are the details of your request.</p>
+                                                                
+                                                                <p><strong>Reservation Details:</strong><br>
+                                                                <b>Business Unit:</b> '.$row['business_unit'].'<br>
+                                                                <b>Room:</b> '.$row['room'].'<br>
+                                                                <b>Contact:</b> '.$row['contact'].'<br>
+                                                                <b>Email:</b> '.$row['email'].'<br>
+                                                                <b>Time:</b> '.$row['time'].'<br>
+                                                                <b>Setup:</b> '.$row['setup'].'<br>
+                                                                <b>Reserved By:</b> '.$row['fname'].' '.$row['lname'].'<br>
+                                                                <b>Reason for Declining : The room is already booked for the selected date and time. Please choose another time or date.
+                                                                </p>
+                                                                <p style="text-align:justify">This is an automated reply from the Reservation System. Please do not reply to this email.</p>
+                                                                <p style="text-align:justify">We look forward to assisting you at the FAST Learning and Development Center. If you have any questions or need further assistance, feel free to contact us at jppsolis@fast.com.ph | Viber Number: +63 969 450 9412.</p>
+                                                                <p style="text-align:justify">Thank you for choosing FAST Learning and Development Center.</p>
+        
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="word-break:break-word;font-size:0px;padding:30px 0px">
+                                                            <p style="font-size:1px;margin:0px auto;border-top:1px solid #dcddde;width:100%"></p>
+                                                        </td>
+                                                    </tr>               
+                                                </tbody>    
+                                            </table>
+                                        </div>  
+                                    </td>
+                                </tr>
+                            </tbody>    
+                        </table>
+                    </div>
+                    <div>
+                        <table align="center">
+                            <tr>
+                            <td style="height:150px; border:none;border-radius:3px;color:black;padding:15px 19px" align="center" valign="middle">&copy; 2024-2025 <strong><span>FAST Learning and Development Center</span></strong></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            ';
+                                          
+    
+            $mail->send();
+        } catch (Exception $e) {
+            error_log("Message could not be sent to $declined_email. Mailer Error: {$mail->ErrorInfo}");
+        }
 
         $sql = $conn->prepare("UPDATE reservations SET reserve_status = :reserve_status, reservation_id = :reservation_id, status_date_created = NOW() AT TIME ZONE 'Asia/Manila' WHERE id = :id ");
         $sql->bindParam(':reserve_status', $approval_status, PDO::PARAM_STR);
@@ -329,11 +464,11 @@ if ($counter->rowCount() > 0) {
             $action = "Reserve Status : Declined | Reserve ID : ". $generateReserveID;
         }
                 // Log the action
-                $user_id = $decrypted_array['ID'];
-                $logs = $conn->prepare("INSERT INTO logs (USER_ID, ACTION_MADE) VALUES (:user_id, :action)");
-                $logs->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                $logs->bindParam(':action', $action, PDO::PARAM_STR);
-                $logs->execute();
+        $user_id = $decrypted_array['ID'];
+        $logs = $conn->prepare("INSERT INTO logs (USER_ID, ACTION_MADE) VALUES (:user_id, :action)");
+        $logs->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+        $logs->bindParam(':action', $action, PDO::PARAM_STR);
+        $logs->execute();
 
         $response['success'] = true;
         $response['title'] = 'Success';
