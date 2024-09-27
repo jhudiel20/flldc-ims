a692a0f232392a52fc26c37140e27f7737a67bec
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-27 19:22:01 +0800

    update

--- api/action/update_reserve_status.php
+++ api/action/update_reserve_status.php
@@ -100,7 +100,7 @@ if ($counter->rowCount() > 0) {
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'lndreports2024@gmail.com';                     //SMTP username
-        $mail->Password   = 'yzmxbjcntuwkfdpe';                               //SMTP password
+        $mail->Password   = $_ENV['EMAIL_PASS'];                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
@@ -365,7 +365,7 @@ if ($counter->rowCount() > 0) {
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'lndreports2024@gmail.com';                     //SMTP username
-                $mail->Password   = 'yzmxbjcntuwkfdpe';                               //SMTP password
+                $mail->Password   = $_ENV['EMAIL_PASS'];                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                           // TCP port to connect to
 
