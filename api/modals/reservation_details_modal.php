806dcb03727a192b6a43fe179843f78b1689887d
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-27 20:52:16 +0800

    .php

--- api/modals/reservation_details_modal.php
+++ api/modals/reservation_details_modal.php
@@ -2,26 +2,26 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalCenterTitle">Are you want to edit the request ?</h5>
+                <h5 class="modal-title" id="modalCenterTitle">Are you sure you want to edit the request ? <br>NOTE: This message will be sent to client</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form method="POST" id="reserve_approval_form">
                 <div class="modal-body">
                     <div class="row">
                         <div class="col mb-3">
-                            <input type="hidden" id="ID" name="ID" value="">
-                            <input type="hidden" id="bookingID" name="ID" value="">
-                            <input type="hidden" id="reserve_status" name="ID" value="">
-                            <input type="hidden" id="reserve_date" name="ID" value="">
-                            <input type="hidden" id="fname" name="ID" value="">
-                            <input type="hidden" id="lname" name="ID" value="">
-                            <input type="hidden" id="time" name="ID" value="">
-                            <input type="hidden" id="setup" name="ID" value="">
-                            <input type="hidden" id="businessunit" name="ID" value="">
-                            <input type="hidden" id="guest" name="ID" value="">
-                            <input type="hidden" id="contact" name="ID" value="">
-                            <input type="hidden" id="email" name="ID" value="">
-                            <input type="hidden" id="message" name="ID" value="">
+                            <input type="hidden" id="ID" name="ID" value="<?php echo $id;?>">
+                            <input type="hidden" id="bookingID" name="ID" value="<?php echo $row['booking_id'];?>">
+                            <input type="hidden" id="reserve_status" name="ID" value="<?php echo $row['reserve_status'];?> ">
+                            <input type="hidden" id="reserve_date" name="ID" value="<?php echo $row['reserve_date'];?>">
+                            <input type="hidden" id="fname" name="ID" value="<?php echo $row['fname']; ?>">
+                            <input type="hidden" id="lname" name="ID" value="<?php echo $row['lname']; ?>">
+                            <input type="hidden" id="time" name="ID" value="<?php echo $row['time']; ?>">
+                            <input type="hidden" id="setup" name="ID" value="<?php echo $row['setup']; ?>">
+                            <input type="hidden" id="businessunit" name="ID" value="<?php echo $row['business_unit']; ?>">
+                            <input type="hidden" id="guest" name="ID" value="<?php echo $row['guest']; ?>">
+                            <input type="hidden" id="contact" name="ID" value="<?php echo $row['contact']; ?>">
+                            <input type="hidden" id="email" name="ID" value="<?php echo $row['email']; ?>">
+                            <input type="hidden" id="message" name="ID" value="<?php echo $row['message']; ?>">
                             <textarea class="form-control" id="reason_message" name="reason_message" rows="5" placeholder="Please provide message"></textarea>
                         </div>
                     </div>
