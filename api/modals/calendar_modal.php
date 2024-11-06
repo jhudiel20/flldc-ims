8ece76932dfcc19771c1629161b33a98160803c1
jhudiel20 <sueltojhudiel20@gmail.com>
2024-11-06 12:32:10 +0800

    update

--- api/modals/calendar_modal.php
+++ api/modals/calendar_modal.php
@@ -7,7 +7,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                        <div class="row">
+                        <div class="row g-2">
                             <div class="col-md-6">
                                 <label class="form-label">Room Name</label>
                                 <input type="text" class="form-control" name="modalRoomName" id="modalRoomName" disabled>
@@ -16,8 +16,7 @@
                                 <label class="form-label">Date</label>
                                 <input type="text" class="form-control" name="modalDate" id="modalDate" disabled>
                             </div>
-                        </div>
-                        <div class="row">
+        
                             <div class="col-md-6">
                                 <label class="form-label">Time</label>
                                 <input type="text" class="form-control" name="modalTime" id="modalTime" disabled>
@@ -26,54 +25,49 @@
                                 <label class="form-label">Seating Arrangement</label>
                                 <input type="text" class="form-control" name="modalSetup" id="modalSetup" disabled>
                             </div>
-                        </div>
-                        <div class="row">
+                     
                             <div class="col-md-12">
                                 <label class="form-label">Requestor</label>
                                 <input type="text" class="form-control" name="modalName" id="modalName" disabled>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
+                      
+                            <div class="col-lg-4">
                                 <label class="form-label">Email<label>
                                 <input type="text" class="form-control" name="modalEmail" id="modalEmail" disabled>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-lg-4">
                                 <label class="form-label">Contact No.</label>
                                 <input type="text" class="form-control" name="modalContact" id="modalContact" disabled>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-lg-4">
                                 <label class="form-label">Bussiness Unit</label>
                                 <input type="text" class="form-control" name="modalBU" id="modalBU" disabled>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label class="form-label">Guest No.<label>
+                 
+                            <div class="col-lg-4">
+                                <label class="form-label">Guest No.</label>
                                 <input type="text" class="form-control" name="modalGuest" id="modalGuest" disabled>
                             </div>
-                            <div class="col-md-4">
-                                <label class="form-label">Hdmi<label>
+                            <div class="col-lg-4">
+                                <label class="form-label">Hdmi</label>
                                 <input type="text" class="form-control" name="modalHdmi" id="modalHdmi" disabled>
                             </div>
-                            <div class="col-md-4">
-                                <label class="form-label">Extension Wire<label>
+                            <div class="col-lg-4">
+                                <label class="form-label">Extension Wire</label>
                                 <input type="text" class="form-control" name="modalExtension" id="modalExtension" disabled>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label class="form-label">Chair No.<label>
+            
+                            <div class="col-lg-6">
+                                <label class="form-label">Chair No.</label>
                                 <input type="text" class="form-control" name="modalChair" id="modalChair" disabled>
                             </div>
-                            <div class="col-md-6">
-                                <label class="form-label">Table No.<label>
+                            <div class="col-lg-6">
+                                <label class="form-label">Table No.</label>
                                 <input type="text" class="form-control" name="modalTable" id="modalTable" disabled>
                             </div>
-                        </div>
-                        <div class="row">
+                 
                             <div class="col-md-12">
-                                <label class="form-label">Purpose / Message<label>
+                                <label class="form-label">Purpose / Message</label>
                                 <textarea class="form-control" name="modalMessage" id="modalMessage" type="text" cols="30" rows="3" disabled></textarea>
                             </div>
                         </div>
