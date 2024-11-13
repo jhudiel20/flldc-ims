24c27546a0f40c033e38bccb336becf793dc68c5
jhudiel20 <sueltojhudiel20@gmail.com>
2024-11-13 17:17:54 +0800

    updates

--- public/change-password.php
+++ public/change-password.php
@@ -67,49 +67,46 @@ $row = $sql->fetch(PDO::FETCH_ASSOC);
                                 <div class="card-body">
                                     <h4 class="mb-2">Reset Password 🔒</h4>
                                     <p class="mb-4">for <span class="fw-medium"><?php echo $row['email'];?></span></p>
-                                    <form id="change_password_form" class="mb-3" method="post">
                                         <input type="hidden" value="<?php echo $code; ?>" id="code" name="code">
-
-                                        <p class="mb-4">Enter your email and we'll send you instructions to reset your password</p>
                                         <form id="change_pass_form" class="mb-3" action="" method="post">
-                                        <div class="mb-3 form-password-toggle">
-                                            <label class="form-label" for="password">New Password</label>
-                                            <div class="input-group input-group-merge">
-                                                <input
-                                                    type="password"
-                                                    id="password"
-                                                    class="form-control"
-                                                    name="password"
-                                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                                    aria-describedby="password" />
-                                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                            <div class="mb-3 form-password-toggle">
+                                                <label class="form-label" for="password">New Password</label>
+                                                <div class="input-group input-group-merge">
+                                                    <input
+                                                        type="password"
+                                                        id="password"
+                                                        class="form-control"
+                                                        name="password"
+                                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                                        aria-describedby="password" />
+                                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="mb-3 form-password-toggle">
-                                            <label class="form-label" for="confirm-password">Confirm Password</label>
-                                            <div class="input-group input-group-merge">
-                                                <input
-                                                    type="password"
-                                                    id="confirmpassword"
-                                                    class="form-control"
-                                                    name="confirmpassword"
-                                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                                    aria-describedby="password" />
-                                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                            <div class="mb-3 form-password-toggle">
+                                                <label class="form-label" for="confirm-password">Confirm Password</label>
+                                                <div class="input-group input-group-merge">
+                                                    <input
+                                                        type="password"
+                                                        id="confirmpassword"
+                                                        class="form-control"
+                                                        name="confirmpassword"
+                                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                                        aria-describedby="password" />
+                                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary d-grid w-100 mb-3" id="set_password">Set new password</button>
-                                        <button class="btn btn-label-primary d-none w-100" type="button" id="set_pass_icon" disabled>
-                                                <span class="spinner-border me-1" role="status" aria-hidden="true"></span>
-                                                Loading...
-                                                </button>
-                                        <div class="text-center">
-                                            <a href="/" class="d-flex align-items-center justify-content-center">
-                                                <i class="bx bx-chevron-left scaleX-n1-rtl bx-sm"></i>
-                                                Back to login
-                                            </a>
-                                        </div>
-                                    </form>
+                                            <button type="submit" class="btn btn-primary d-grid w-100 mb-3" id="set_password">Set new password</button>
+                                            <button class="btn btn-label-primary d-none w-100" type="button" id="set_pass_icon" disabled>
+                                                    <span class="spinner-border me-1" role="status" aria-hidden="true"></span>
+                                                    Loading...
+                                                    </button>
+                                            <div class="text-center">
+                                                <a href="/" class="d-flex align-items-center justify-content-center">
+                                                    <i class="bx bx-chevron-left scaleX-n1-rtl bx-sm"></i>
+                                                    Back to login
+                                                </a>
+                                            </div>
+                                        </form>
                                 </div>
                             </div>
                         </div>
