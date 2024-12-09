f19f22c6c5bc9f1800dec5f688c76855eedac17a
jhudiel20 <sueltojhudiel20@gmail.com>
2024-12-09 22:09:53 +0800

    updates

--- api/modals/user_list_modal.php
+++ api/modals/user_list_modal.php
@@ -89,7 +89,7 @@
 
 <div class="modal fade" id="change_room_access_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Please select the reservation access for the user (<span id="user"></span>) ?</h5>
