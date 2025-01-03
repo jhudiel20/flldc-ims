394f94b196f138873d05db6c0c6c18aec74944b4
jhudiel20 <sueltojhudiel20@gmail.com>
2025-01-03 12:05:21 +0800

    update

--- api/action/update_reserve_status.php
+++ api/action/update_reserve_status.php
@@ -143,28 +143,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>Bill Invoice</title>
-                                    <style>
-                    .row {
-                        display: flex;
-                        flex-wrap: wrap;
-                        margin-bottom: 20px;
-                    }
-                    .col-1 { width: 8.33%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-2 { width: 16.66%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-3 { width: 25%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-4 { width: 33.33%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-5 { width: 41.66%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-6 { width: 50%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-7 { width: 58.33%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-8 { width: 66.66%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-9 { width: 75%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-10 { width: 83.33%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-11 { width: 91.66%; float: left; padding: 2px; box-sizing: border-box; }
-                    .col-12 { width: 100%; float: left; padding: 2px; box-sizing: border-box; }
                 </style>
                 </head>
                 <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
-                    <div style="width: 90%; margin: auto;">
+                    <div style="width: 90%;">
                         <div style="font-size: 16px; text-align: center; font-weight: bold; margin-bottom: 40px;">
                             SERVICE INVOICE
                         </div>
