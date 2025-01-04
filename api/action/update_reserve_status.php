f14e8448e29e6913f647c041e6bc9132968c14df
jhudiel20 <sueltojhudiel20@gmail.com>
2025-01-04 15:19:44 +0800

    update

--- api/action/update_reserve_status.php
+++ api/action/update_reserve_status.php
@@ -147,10 +147,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </head>
                 <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
                     <div style="width: 90%;">
-                        <div style="font-size: 16px; text-align: center; font-weight: bold; padding-bottom: 40px;">
+                        <div style="font-size: 16px; text-align: center; font-weight: bold;">
                             SERVICE INVOICE
                         </div>
-                        <table style="border: 1px solid white; padding: 20px; width: 100%;">
+                        <table style="border: 1px solid white; padding-top: 40px; padding-bottom: 20px; width: 100%;">
                             <tr>
                                 <th style="border: 1px solid white;">
                                     <strong>INVOICE-' . $generateReserveID . '</strong>
@@ -160,7 +160,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 </th>
                             </tr>
                         </table>
-                        <div style="padding-top: 20px; margin-bottom: 20px;">
+                        <div style="margin-bottom: 20px;">
                             <strong>FAST LOGISTICS LEARNING AND DEVELOPMENT CORPORATION</strong><br>
                             Fast Warehouse Complex, Pulo-Diezmo Road,<br>
                             Barangay Pulo, Cabuyao City Laguna.
