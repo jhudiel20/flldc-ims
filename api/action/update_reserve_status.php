46ed42cb9b2030912970191056d8cbe0f7601b32
jhudiel20 <sueltojhudiel20@gmail.com>
2025-01-04 19:10:04 +0800

    update

--- api/action/update_reserve_status.php
+++ api/action/update_reserve_status.php
@@ -137,6 +137,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </head>
                 <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
                     <div style="width: 90%;">
+                        <img src="cid:logo_cid" style="height: 40px; margin: 0 auto; display: inline;">
+                        <img src="/../../public/assets/img/LOGO.png" style="height: 40px; margin: 0 auto; display: inline;">
+                        <img src="/../../assets/img/LOGO.png" style="height: 40px; margin: 0 auto; display: inline;">
+                        <img src="/LOGO.png" style="height: 40px; margin: 0 auto; display: inline;">
+
                         <div style="font-size: 16px; text-align: center; font-weight: bold;">
                             SERVICE INVOICE
                         </div>
